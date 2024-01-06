5ea9784a329561d16abbd659dea7b22e13ecfab1
Ivan Ivanov <test0test1011000@gmail.com>
2024-01-06 12:06:23 +0300

    Done

---
+++ src/test/java/praktikum/constants/NamesConst.java
@@ -0,0 +1,7 @@
+package praktikum.constants;
+
+public class IngredientsConst
+{
+    public static final String FIRST_INGREDIENT_NAME = "Мазик";
+    public static final String SECOND_INGREDIENT_NAME = "Котлета";
+}
---
+++ src/test/java/praktikum/constants/PriceConst.java
@@ -0,0 +1,2 @@
+package praktikum.constants;public class PriceConst {
+}
